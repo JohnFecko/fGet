fac6c60702f85e2b7a5f60716e5482ccd08445dc
John Fecko <john@fecko.me>
2014-03-09 17:08:23 -0400

    Initial commit

---
+++ fGet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(DownloadUrl(args[0]));
+            }
+        }
+
+        public static string DownloadUrl(string url)
+        {
+            string sContents = string.Empty;
+            try
+            {
+                if (url.ToLower().IndexOf("http:") > -1)
+                {
+                    System.Net.WebClient wc = new System.Net.WebClient();
+                    byte[] response = wc.DownloadData(url);
+                    sContents = System.Text.Encoding.ASCII.GetString(response);
+                }
+            }
+            catch (Exception)
+            {
+                sContents = "";
+            }
+            return sContents;
+        }
+    }
+}
